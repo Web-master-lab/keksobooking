ae000a5dfda3709a27d4ae777094340c3a8c457a
Gleb <stepanov-gleb2003@mail.ru>
2023-07-16 08:15:11 +0300

    Сделал показ сообщения при отправке формы. Перенес код карты сюда

--- js/form.js
+++ js/form.js
@@ -1,3 +1,11 @@
+import { toggleToActive, toggleToDisable, showMessage } from './util.js';
+import { createCard } from './create-cards.js';
+
+const CENTER_COORDINATE = {
+  lat: 35.681729,
+  lng: 139.753927
+};
+
 const MAX_VALUE = 100000;
 
 const TYPES = {
@@ -15,14 +23,21 @@ const ROOMS_LIMITS = {
   100: [0]
 };
 
+const address = document.querySelector('#address');
 const form = document.querySelector('.ad-form');
 const price = form.querySelector('#price');
 const type = document.querySelector('#type');
+const title = document.querySelector('#title');
 const rooms = document.querySelector('#room_number');
 const guests = document.querySelector('#capacity');
 const timein = document.querySelector('#timein');
 const timeout = document.querySelector('#timeout');
 const sliderElement = document.querySelector('.ad-form__slider');
+const reset = document.querySelector('.ad-form__reset');
+const description = document.querySelector('#description');
+const features = document.querySelectorAll('.features__checkbox');
+const images = document.querySelector('#images');
+const avatar = document.querySelector('#avatar');
 
 noUiSlider.create(sliderElement, {
   range: {
@@ -46,18 +61,154 @@ sliderElement.noUiSlider.on('update', () => {
 });
 
 const pristine = new Pristine(form, {
-  classTo: 'ad-form',
-  errorTextParent: 'ad-form',
-  errorTextClass: 'error__message',
+  classTo: 'ad-form__element',
+  errorTextParent: 'ad-form__element',
+  errorTextTag: 'span',
+  errorTextClass: 'ad-form__error',
 });
+
+document.addEventListener('load', toggleToDisable);
+
+const map = L.map('map-canvas')
+  .on('load', () => {
+    toggleToActive();
+    address.value = `${CENTER_COORDINATE.lat}, ${CENTER_COORDINATE.lng}`;
+  })
+  .setView({
+    lat: CENTER_COORDINATE.lat,
+    lng: CENTER_COORDINATE.lng
+  }, 10);
+
+L.tileLayer(
+  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  },)
+  .addTo(map);
+
+const mainPinIcon = L.icon({
+  iconUrl: './img/main-pin.svg',
+  iconSize: [52, 52],
+  iconAnchor: [26, 52]
+});
+
+const mainPinMarker = L.marker(
+  {
+    lat: CENTER_COORDINATE.lat,
+    lng: CENTER_COORDINATE.lng
+  },
+  {
+    draggable: true,
+    icon: mainPinIcon
+  })
+  .addTo(map);
+
+mainPinMarker.on('moveend', (evt) => {
+  address.value = `${evt.target.getLatLng().lat.toFixed(5)}, ${evt.target.getLatLng().lng.toFixed(5)}`;
+});
+
+const regularPinIcon = L.icon({
+  iconUrl: './img/pin.svg',
+  iconSize: [40, 40],
+  iconAnchor: [20, 40]
+});
+
+fetch('https://25.javascript.pages.academy/keksobooking/data')
+  .then((response) => response.json())
+  .then((ads) => {
+    ads.forEach((ad) => {
+      const regularPinMarker = L.marker(
+        {
+          lat: ad.location.lat,
+          lng: ad.location.lng
+        },
+        {
+          icon: regularPinIcon
+        }
+      );
+      regularPinMarker
+        .addTo(map)
+        .bindPopup(createCard(ad));
+    });
+  })
+  .catch(() => {
+    const errorElement = document.createElement('div');
+    errorElement.textContent = 'Ошибка загрузки данных с сервера';
+    errorElement.classList.add('error__message--fetch');
+    document.body.appendChild(errorElement);
+
+    const button = document.createElement('button');
+    button.textContent = 'Хорошо';
+    button.classList.add('error__button');
+    button.classList.add('error__button--fetch');
+    errorElement.appendChild(button);
+
+    button.addEventListener('click', () => errorElement.remove());
+  });
+
+const clearForm = () => {
+  sliderElement.noUiSlider.updateOptions({
+    range: {
+      min: 0,
+      max: 100000
+    },
+    start: 0,
+    step: 1,
+    format: {
+      to: function (value) {
+        return value.toFixed(0);
+      },
+      from: function (value) {
+        return value;
+      },
+    }
+  });
+
+  mainPinMarker.setLatLng({
+    lat: CENTER_COORDINATE.lat,
+    lng: CENTER_COORDINATE.lng
+  });
+
+  address.value = `${CENTER_COORDINATE.lat}, ${CENTER_COORDINATE.lng}`;
+  title.value = '';
+  price.value = 0;
+  rooms.value = 1;
+  guests.value = 3;
+  type.value = 'flat';
+  timein.value = '12:00';
+  timeout.value = '12:00';
+  description.value = '';
+  features.forEach((feature) => {feature.checked = false;});
+  images.value = '';
+  avatar.value = '';
+};
 
 form.addEventListener('submit', (evt) => {
   evt.preventDefault();
   const isValid = pristine.validate();
-  return isValid;
+  if (isValid) {
+    const formData = new FormData(evt.target);
+
+    fetch('https://25.javascript.pages.academy/keksobooking',
+      {
+        method: 'POST',
+        body: formData
+      }
+    ).then(() => {
+      showMessage(true);
+      clearForm();
+    })
+      .catch(() => {showMessage(false);});
+  }
+});
+
+reset.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  clearForm();
 });
 
 type.addEventListener('change', () => {
+  price.min = TYPES[type.value];
   price.placeholder = TYPES[type.value];
 });
 
@@ -81,5 +232,5 @@ const validateRooms = () => {
   return allowedGuests.includes(selectedGuests);
 };
 
-pristine.addValidator(guests, validateRooms, 'Неверное количество гостей');
-pristine.addValidator(price, validatePrice, 'Неверная сумма');
+pristine.addValidator(guests, validateRooms, 'На каждого гостя должна быть 1 комната');
+pristine.addValidator(price, validatePrice, 'Неверная цена');
